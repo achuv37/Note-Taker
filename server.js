7989718f1a85269f23546ebe55b33a1daeabc047
Achu Ajesh <achuv37@gmail.com>
2022-04-21 22:11:54 -0400

    add route for deleting notes

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 const path = require('path');
 // Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 4900;
 
 const app = express();
-
+// Middleware
 // Parse incoming string or array data
 app.use(express.urlencoded({ extended: true}));
 // Parse incoming JSON data
@@ -20,28 +20,40 @@ app.get('/notes', (req,res ) => {
 });
 
 // Get request for api/notes
+// This gets notes saved and joins it in db.json
 app.get('/api/notes' , (req, res) => {
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    var notes = JSON.parse(data);
-  });
+  res.sendFile(path.join(__dirname, '/db/db.json'));
 });
+
 // post request for adding notes
 app.post('/api/notes' , (req, res) => {
-  fs.readFile('./db/db.json', 'utf8' , (err,data) => {
-    if (err) throw err;
-    var notes = JSON.parse(data);
-    let newNote = req.body;
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNote = req.body;
     newNote.id = uuid();
     notes.push(newNote);
     const noteString = JSON.stringify(notes);
-    fs.writeFile('./db/db.json', noteString , (err, data)  => {
-      res.json(newNote);
+    fs.writeFileSync('./db/db.json', noteString , (err, data)  => {
+      res.json(notes);
     });
   });
+
+// Deleting notes
+app.delete("/api/notes/:id" , (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+  const delNote = notes.filter((note) => note.id !== req.params.id);
+  const noteString = JSON.stringify(delNote);
+  fs.writeFileSync('./db/db.json', noteString , (err, data)  => {
+    res.json(delNote);
+});
 });
 
 
+
+// Home page
+app.get('/', (req, res) => {
+  console.log(req.method);
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+})
 
 
 // Express-js wildcard route
