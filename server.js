81d9b3784d6785d17187b3662fd73e5c1b8bf25e
Achu Ajesh <achuv37@gmail.com>
2022-04-21 21:02:59 -0400

    Add post and get request

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
-const PORT = process.env.PORT || 3001;
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+const PORT = process.env.PORT || 5001;
 
 const app = express();
 
@@ -15,8 +16,40 @@ app.use(express.static('public'));
 // Get request for notes.
 app.get('/notes', (req,res ) => {
   console.log(req.method);
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
+
+// Get request for api/notes
+app.get('/api/notes' , (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+  });
+});
+// post request for adding notes
+app.post('/api/notes' , (req, res) => {
+  fs.readFile('./db/db.json', 'utf8' , (err,data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    let newNote = req.body;
+    newNote.id = uuid();
+    notes.push(newNote);
+    const noteString = JSON.stringify(notes);
+    fs.writeFile('./db/db.json', noteString , (err, data)  => {
+      res.json(newNote);
+    });
+  });
+});
+
+
+
+
+// Express-js wildcard route
+app.get('*', (req, res) => {
+  console.log(req.method);
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
 
 
 
